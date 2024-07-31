54a1526470db22a5680cc77bc77baf4937c7a80c
clydefritz <clyde@clydefritz.com>
2024-07-31 16:44:32 -0400

    updated model to use flex-function-fw-index.js as main entry point

---
+++ flex-function-fw-index.js
@@ -0,0 +1,12 @@
+const { flexInitializeEnvVars, flexGetEnvVars, flexAddEnvVariable, flexGetEnvVariable } = require('../lib/flex-fw-env-utils');
+
+
+
+
+// Export all functions
+module.exports = {
+    flexInitializeEnvVars,
+    flexGetEnvVars,
+    flexAddEnvVariable,
+    flexGetEnvVariable
+}
