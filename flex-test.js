4fd82a5f757ec5c019aa49d00814aee6aa501bec
clydefritz <clyde@clydefritz.com>
2024-08-03 04:04:13 -0400

    added first gcp function start

--- flex-test.js
+++ flex-test.js
@@ -1,7 +1,12 @@
-const flexFunction = require('./flex-function-fw-index');
+const flexFunctionFw = require('./flex-function-fw-index');
+var gcpfunctions_fw = require('@google-cloud/functions-framework');
+let thisFlexFunction = undefined;
 
 async function flexFunctionMain() {
-   await flexFunction.flexFunctionStart();
+   thisFlexFunction = await flexFunctionFw.flexFunctionStart();
+   // Register an HTTP function with the Functions Framework that will be executed
+   // when you make an HTTP request to the deployed function's endpoint.
+   gcpfunctions_fw.http('default-function', thisFlexFunction);
 }
 
 flexFunctionMain();
